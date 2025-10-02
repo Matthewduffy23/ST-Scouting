2596fd1386ce9599b6342b71d552b9a6c119d999
Matthewduffy23 <matthewduffy2000@gmail.com>
2025-10-02 14:29:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1777,7 +1777,7 @@ else:
             x_right_edge = 1 - RIGHT
             x = x_right_edge - (right_index + 1) * img_box_w - right_index * img_gap
             # Nudge middle (index 1) and left (index 2) to the RIGHT
-            per_image_shift = {0: 0.02, 1: 0.06, 2: 0.10}  # tweak these values
+            per_image_shift = {0: 0.02, 1: 0.04, 2: 0.10}  # tweak these values
             x += per_image_shift.get(right_index, 0.0)
             y_top_band = 1 - TOP - 0.006
             y = y_top_band - img_box_h
